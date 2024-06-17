38a62e53433c6d502ce6c134cde8b739e2098e63
Martin Diaz Preiero <martinpreierodiaz@gmail.com>
2024-06-17 10:41:26 -0300

    Creacion de Models, TokenModel y UsuarioModel

---
+++ ObligatorioP3_frontend/Models/TokenModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ObligatorioP3_frontend.Models
+{
+    public class TokenModel
+    {
+        public string Token { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ ObligatorioP3_frontend/Models/UsuarioModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Diagnostics.Contracts;
+
+namespace ObligatorioP3_frontend.Models
+{
+    public class UsuarioModel
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Clave { get; set; }
+        public string ClaveSinEncriptar { get; set; }
+
+    }
+}
