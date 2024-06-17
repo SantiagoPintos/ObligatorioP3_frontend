4820d7fb67687068dedd83608ec51a2f08f650a0
Martin Diaz Preiero <martinpreierodiaz@gmail.com>
2024-06-17 10:40:53 -0300

    HomeController: Creacion de ActionResult Login e Index

--- ObligatorioP3_frontend/Controllers/HomeController.cs
+++ ObligatorioP3_frontend/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+using Newtonsoft.Json;
 using ObligatorioP3_frontend.Models;
 using System.Diagnostics;
+using System.Reflection;
+using System.Text;
 
 namespace ObligatorioP3_frontend.Controllers
 {
@@ -15,18 +19,48 @@ namespace ObligatorioP3_frontend.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            HttpClient cliente = new HttpClient();
+            Uri uri = new Uri("http://localhost:5029/Login");
+            HttpRequestMessage solicitud = new HttpRequestMessage(HttpMethod.Get, uri);                                    
+            Task<HttpResponseMessage> respuesta = cliente.SendAsync(solicitud);
+            respuesta.Wait();
+            if (respuesta.Result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Usuario");
+            }           
+            return RedirectToAction("Login", "Home");
+            
         }
 
-        public IActionResult Privacy()
+        public IActionResult Login()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [HttpPost]
+        public IActionResult Login(string email, string clave)
+        {            
+            HttpClient cliente = new HttpClient();
+            Uri uri = new Uri("http://localhost:5029/api/Login/Login");
+            HttpRequestMessage solicitud = new HttpRequestMessage(HttpMethod.Post, uri);            
+            UsuarioModel model = new UsuarioModel();
+            model.Email = email;
+            model.Clave = clave;
+            var json = JsonConvert.SerializeObject(model);
+            HttpContent contenido = new StringContent(json, Encoding.UTF8, "application/json");
+            solicitud.Content = contenido;
+            Task<HttpResponseMessage> respuesta = cliente.SendAsync(solicitud);
+            respuesta.Wait();
+            if (respuesta.Result.IsSuccessStatusCode)
+            {
+                var objetoComoTexto = respuesta.Result.Content.ReadAsStringAsync().Result;
+                var usuario = JsonConvert.DeserializeObject<TokenModel>(objetoComoTexto);
+                HttpContext.Session.SetString("Email", email);
+                HttpContext.Session.SetString("Token", usuario.Token);
+                return RedirectToAction("Index", "Usuario");
+            }
+
+            return View();
         }
     }
 }
