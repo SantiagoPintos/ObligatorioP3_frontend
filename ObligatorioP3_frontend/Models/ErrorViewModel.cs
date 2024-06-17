f272fe243cdf234392b6f33ca4d2d64159ff77c0
Santiago Pintos <spintos@outlook.es>
2024-06-17 08:42:38 -0300

    Commit inicial
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

---
+++ ObligatorioP3_frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ObligatorioP3_frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
