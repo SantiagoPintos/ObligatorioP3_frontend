dd96717cc44e10695b70a194af598d7f1208977d
Martin Diaz Preiero <martinpreierodiaz@gmail.com>
2024-06-17 10:41:07 -0300

    UsuarioController: Creacion de UsuarioController

---
+++ ObligatorioP3_frontend/Controllers/UsuarioController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ObligatorioP3_frontend.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: UsuarioController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
