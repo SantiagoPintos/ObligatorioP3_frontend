b6e90924f07df91644ab2b91770136ac1a30a12d
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-06-17 21:31:25 -0300

    Creacion de ArticuloModel

---
+++ ObligatorioP3_frontend/Models/ArticuloModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ObligatorioP3_frontend.Models
+{
+    public class ArticuloModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public long Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public int stock { get; set; }
+        public decimal PrecioUnitario { get; set; }
+    }
+}
