fe95097564de5ad83fb2867dcc945ee16291f139
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-06-18 17:39:14 -0300

    Creacion de ResumenModel y SettingsModel

---
+++ ObligatorioP3_frontend/Models/ResumenModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ObligatorioP3_frontend.Models
+{
+    public class ResumenModel
+    {
+        public int año { get; set; }
+        public TipoMovimientoo[] tipoMovimiento { get; set; }
+        public int suma { get; set; }
+    }
+    public class TipoMovimientoo
+    {
+        public string Nombre { get; set; }
+        public int cantidad { get; set; }
+    }
+}
---
+++ ObligatorioP3_frontend/Models/SettingsModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ObligatorioP3_frontend.Models
+{
+    public class SettingsModel
+    {
+        public string Nombre { get; set; }
+        public double Valor { get; set; }
+    }
+}
