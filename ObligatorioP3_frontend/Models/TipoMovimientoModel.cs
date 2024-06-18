fa54c8591c9474356c7d6301ed4aba88ddee410f
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-06-17 21:31:46 -0300

    Creacion de TipoMovimientoModel

---
+++ ObligatorioP3_frontend/Models/TipoMovimientoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ObligatorioP3_frontend.Models
+{
+    public class TipoMovimientoModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Signo { get; set; }
+    }
+}
