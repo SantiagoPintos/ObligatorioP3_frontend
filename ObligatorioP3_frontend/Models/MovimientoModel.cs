bdf4ac6aee4c63ff92fc97d6648bd8f4f23e7a60
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-06-17 21:31:35 -0300

    Creacion de Movimiento Model

---
+++ ObligatorioP3_frontend/Models/MovimientoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ObligatorioP3_frontend.Models
+{
+    public class MovimientoModel
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public ArticuloModel Articulo { get; set; }
+        public string Usuario { get; set; }
+        public int Cantidad { get; set; }
+        public TipoMovimientoModel TipoMovimiento { get; set; }
+    }
+}
